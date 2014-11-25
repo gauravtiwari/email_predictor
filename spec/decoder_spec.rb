cb40cafa154c28533d1a55eb720749ab5e216845
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2014-11-25 15:43:48 +0000

    Modified tests to match the respective class methods

--- spec/decoder_spec.rb
+++ spec/decoder_spec.rb
@@ -1,18 +1,11 @@
 require 'email_predictor/formatters/decoder'
+require File.expand_path("../../data/data", __FILE__)
 
 module EmailPredictor
   describe Formatters::Decoder do
-    context 'I want to extract attributes from a given email' do
+    context 'I want to decode attributes from a given email' do
 
-      let(:given_formats) do
-        [ -> (name) { name.join('.') },
-          -> (name) { name.first + '.' + name.last[0] },
-          -> (name) { name.first[0] + '.' + name.last[0] },
-          -> (name) { name.first[0] + '.' + name.last }
-        ]
-        end
-
-        let(:decoder) { EmailPredictor::Formatters::Decoder.new given_formats }
+        let(:decoder) { EmailPredictor::Formatters::Decoder.new GIVEN_FORMATS }
 
         it 'should match to first_name.last_name@domain.com email format' do
           name = 'John Ferguson'
@@ -20,28 +13,28 @@ module EmailPredictor
           decoder.decode(email, name)
           expect(decoder.email_domain).to eq 'alphasights.com'
           expect(decoder.name).to eq ['john', 'ferguson']
-          expect(decoder.email_format.first).to eq given_formats[0]
+          expect(decoder.email_format.first).to eq GIVEN_FORMATS[0]
         end
 
         it 'should match to first_name.last_initial@domain.com email format' do
           name = 'John Ferguson'
           email = 'john.f@alphasights.com'
           decoder.decode(email, name)
-          expect(decoder.email_format.first).to eq given_formats[1]
+          expect(decoder.email_format.first).to eq GIVEN_FORMATS[1]
         end
 
         it 'should match to first_initial.last_initial@domain.com email format' do
           name = 'John Ferguson'
           email = 'j.f@alphasights.com'
           decoder.decode(email, name)
-          expect(decoder.email_format.first).to eq given_formats[2]
+          expect(decoder.email_format.first).to eq GIVEN_FORMATS[2]
         end
 
         it 'should match to first_initial.last_name@domain.com email format' do
           name = 'John Ferguson'
           email = 'j.ferguson@alphasights.com'
           decoder.decode(email, name)
-          expect(decoder.email_format.first).to eq given_formats[3]
+          expect(decoder.email_format.first).to eq GIVEN_FORMATS[3]
         end
 
         it 'should match to no email format' do
--- spec/encoder_spec.rb
+++ spec/encoder_spec.rb
@@ -1,42 +1,35 @@
 require 'email_predictor/formatters/encoder'
+require File.expand_path("../../data/data", __FILE__)
 
 module EmailPredictor
   describe Formatters::Encoder do
     let(:name) { 'John Ferguson' }
     let(:domain) { 'alphasights.com' }
-    let(:given_formats) do
-      [
-        -> (name) { name.join('.') },
-        -> (name) { name.first + '.' + name.last[0] },
-        -> (name) { name.first[0] + '.' + name.last[0] },
-        -> (name) { name.first[0] + '.' + name.last }
-      ]
-    end
 
-    context 'I want to format an email addresses' do
+    context 'I want to encode an email addresses' do
       it 'should match to first.last@domain.com email format' do
-        email_format = given_formats[0]
+        email_format = GIVEN_FORMATS[0]
         encoder = EmailPredictor::Formatters::Encoder.new(email_format, domain)
         encoder.encode(name)
         expect(encoder.email).to eq 'john.ferguson@alphasights.com'
       end
 
       it 'should match to first.l@domain.com email format' do
-        email_format = given_formats[1]
+        email_format = GIVEN_FORMATS[1]
         encoder = EmailPredictor::Formatters::Encoder.new(email_format, domain)
         encoder.encode(name)
         expect(encoder.email).to eq 'john.f@alphasights.com'
       end
 
       it 'should match to f.l@domain.com email format' do
-        email_format = given_formats[2]
+        email_format = GIVEN_FORMATS[2]
         encoder = EmailPredictor::Formatters::Encoder.new(email_format, domain)
         encoder.encode(name)
         expect(encoder.email).to eq 'j.f@alphasights.com'
       end
 
       it 'should match to f.last@domain.com email format' do
-        email_format = given_formats[3]
+        email_format = GIVEN_FORMATS[3]
         encoder = EmailPredictor::Formatters::Encoder.new(email_format, domain)
         encoder.encode(name)
         expect(encoder.email).to eq 'j.ferguson@alphasights.com'
