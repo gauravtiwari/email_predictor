b3f37b2f40549b79d7215d15bc10527d0709ec57
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2014-11-25 14:05:36 +0000

    inspector interface to inspect various email formats - entry point to decode formats

---
+++ lib/email_predictor/inspector.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+module EmailPredictor
+  class Inspector
+
+    attr_accessor :formats
+    attr_reader :data, :decoder
+
+    def initialize(data, decoder)
+      @formats = {}
+      @data = data
+      @decoder = decoder
+    end
+
+    def decode_formats
+      data.each do |name, email|
+        decoder.decode(email, name)
+        formats[decoder.email_domain] ||= decoder.email_format
+        formats[decoder.email_domain] += decoder.email_format
+        formats[decoder.email_domain].uniq!
+      end
+    end
+  end
+end
