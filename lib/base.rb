8ddb97e7ed755f0f8f35d0f7d7c250edac591c24
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2014-11-25 16:19:28 +0000

    Base Class to interface with EmailPridictor module

---
+++ lib/base.rb
@@ -0,0 +1,23 @@
+class Base
+  attr_accessor  :decoder, :given_formats,:inspector, :predictions
+
+  def initialize(given_formats)
+    @decoder = EmailPredictor::Formatters::Decoder.new(given_formats)
+  end
+
+  def context(data)
+    @inspector = EmailPredictor::Inspector.new(data, decoder)
+    self.inspector.decode_formats
+  end
+
+  # Predict array of emails based on patterns and clients.
+  # @param name
+  # @param domain
+  # @return [Array]
+
+  def predict(name, domain)
+    return 'Can\'t predict email formats for this data'  unless inspector.formats[domain]
+    encoders = inspector.formats[domain].map {|p| EmailPredictor::Formatters::Encoder.new(p, domain) }
+    encoders.map {|encoder| encoder.encode(name) }
+  end
+end
